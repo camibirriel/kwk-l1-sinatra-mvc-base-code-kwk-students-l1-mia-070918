dc51129e5b0c566085bea8daa55c7c45564bad00
camibirriel <camibirriel@gmail.com>
2018-07-17 17:16:55 +0000

    automatically backed up by learn

--- application_controller.rb
+++ application_controller.rb
@@ -1,10 +1,16 @@
 require 'bundler'
+require './models/model'
 Bundler.require
 
 class MyApp < Sinatra::Base
+get "/" do
+return erb:index
+end
 
-  get '/' do
-    erb :index
-  end
+post "/" do
+  @name = params[:user]
+  @fortune = get_fortune
+  return erb :results 
+end
 
 end
---
+++ models/model.rb
@@ -0,0 +1,5 @@
+def get_fortune
+  fortunes_array = ["you will prosper", "you will win the vegetable off today", "you will meet karlie kloss", "you will become the greatest computer scientist of all time", "your crush will fall in love with you"]
+  return fortunes_array.sample
+
+end
